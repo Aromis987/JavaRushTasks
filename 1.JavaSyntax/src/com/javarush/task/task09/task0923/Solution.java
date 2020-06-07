13cc85b631e6a27ff2215acfe344ee337537364c
Aromis <g.ermilov89@gmail.com>
2020-06-07 20:45:33 +0300

    Написать программу, которая вводит с клавиатуры строку текста. Программа должна вывести на экран две строки: 1. первая строка содержит только гласные буквы из введённой строки. 2. вторая - только согласные буквы и знаки препинания из введённой строки. Буквы соединять пробелом, каждая строка должна заканчиваться пробелом.

---
+++ 1.JavaSyntax/src/com/javarush/task/task09/task0923/Solution.java
@@ -0,0 +1,42 @@
+package com.javarush.task.task09.task0923;
+
+/* 
+Гласные и согласные
+*/
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Solution {
+    public static char[] vowels = new char[]{'а', 'я', 'у', 'ю', 'и', 'ы', 'э', 'е', 'о', 'ё'};
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String word = reader.readLine();
+        char[] chars = word.toCharArray();
+        for (char aChar : chars) {
+            if (isVowel(aChar)) {
+                System.out.print(aChar + " ");
+            }
+        }
+
+        System.out.println();
+
+        for (char aChar : chars) {
+            if (!isVowel(aChar) && aChar != ' ') {
+                System.out.print(aChar + " ");
+            }
+        }
+    }
+
+    // метод проверяет, гласная ли буква
+    public static boolean isVowel(char c) {
+        c = Character.toLowerCase(c);  // приводим символ в нижний регистр - от заглавных к строчным буквам
+        for (char d : vowels) {  // ищем среди массива гласных
+            if (c == d) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
